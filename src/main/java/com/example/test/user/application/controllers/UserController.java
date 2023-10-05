467b994cbef886b0678025b814529bb076f06339
om <pleasedontcry@pm.me>
2023-10-06 01:34:34 +0200

    feat: removed the comments from jwtService

--- src/main/java/com/example/test/user/application/controllers/UserController.java
+++ src/main/java/com/example/test/user/application/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.test.user.application.controllers;
 
 import com.example.test.user.application.dto.WithdrawDto;
 import com.example.test.user.application.dto.WithdrawRequestBody;
+import com.example.test.user.application.services.JwtService;
 import com.example.test.user.application.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,9 @@ public class UserController {
 
     @PatchMapping("/withdraw-credits/{userId}")
     public int withdrawCredits(@PathVariable UUID userId, @RequestBody WithdrawRequestBody body, @RequestHeader("Authorization") String bearerToken) {
-//            String token = bearerToken.substring(7);
+            String token = bearerToken.substring(7);
 
-//            JwtService.validateAndReadJwt(token);
+            JwtService.validateAndReadJwt(token);
 
             return this.userService.withdrawCredits(new WithdrawDto(userId, body.getCredits()));
         }
